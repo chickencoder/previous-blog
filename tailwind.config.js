15434588707627fe10b89d195441fba8fda6597e
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-04-13 21:23:20 +0100

    Layout stuff mainly :bikiki:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
-module.exports = {}
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Inter', 'system-ui', 'sans-serif']
+      },
+      colors: {
+        fish: '#015adf',
+        sea: '#4DFFD4',
+      }
+    }
+  }
+}
